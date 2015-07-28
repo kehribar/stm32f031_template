b9a8b0354036449ada547d17c633726c85e12834
kehribar <ihsan@kehribar.me>
2015-07-29 01:56:27 +0300

    Style fix.

--- main.c
+++ main.c
@@ -1,39 +1,40 @@
-
+/*-----------------------------------------------------------------------------
+/
+/
+/----------------------------------------------------------------------------*/
 #include "stm32f0xx.h"
 #include "stm32f0xx_gpio.h"
 #include "stm32f0xx_rcc.h"
 
 int main(void)
 {
-	volatile int i = 0;
-	GPIO_InitTypeDef InitGpio;
+  volatile int i = 0;
 
-	RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
+  GPIO_InitTypeDef InitGpio;
 
-	// Configures the GPIOC pin8 and pin9, since leds are connected
-	// to PC8 and PC9 of GPIOC
-	InitGpio.GPIO_Pin = GPIO_Pin_5;
-	InitGpio.GPIO_Mode = GPIO_Mode_OUT;
-	InitGpio.GPIO_Speed = GPIO_Speed_Level_1;
-	InitGpio.GPIO_OType = GPIO_OType_PP;
-	InitGpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
+  RCC_AHBPeriphClockCmd(RCC_AHBPeriph_GPIOA, ENABLE);
 
-	// Initialises the GPIOC
-	GPIO_Init(GPIOA, &InitGpio);
+  InitGpio.GPIO_Pin = GPIO_Pin_5;
+  InitGpio.GPIO_Mode = GPIO_Mode_OUT;
+  InitGpio.GPIO_Speed = GPIO_Speed_Level_1;
+  InitGpio.GPIO_OType = GPIO_OType_PP;
+  InitGpio.GPIO_PuPd = GPIO_PuPd_NOPULL;
+
+  GPIO_Init(GPIOA, &InitGpio);
 
   while(1)
   {
-		for(i=0;i<50000;i++);
-		GPIO_ResetBits(GPIOA,GPIO_Pin_5);
+    for(i=0;i<50000;i++);
+    GPIO_ResetBits(GPIOA,GPIO_Pin_5);
 
-		for(i=0;i<50000;i++);
-		GPIO_SetBits(GPIOA,GPIO_Pin_5);
+    for(i=0;i<50000;i++);
+    GPIO_SetBits(GPIOA,GPIO_Pin_5);
 
-		for(i=0;i<50000;i++);
-		GPIO_ResetBits(GPIOA,GPIO_Pin_5);
+    for(i=0;i<50000;i++);
+    GPIO_ResetBits(GPIOA,GPIO_Pin_5);
 
-		for(i=0;i<500000;i++);
-		GPIO_SetBits(GPIOA,GPIO_Pin_5);
+    for(i=0;i<500000;i++);
+    GPIO_SetBits(GPIOA,GPIO_Pin_5);
   } 
 
   return 0;
